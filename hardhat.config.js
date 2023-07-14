bb0f4551b5cc9683d228ca49f7ce30c485dc2ccc
sylverb <sylver.bruneau@gmail.com>
2023-07-14 08:57:19 +0200

    Enable solidity compiler optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,15 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    version: '0.8.18',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    },
+  },
   networks: {
     hardhat: {},
     mumbai: {
