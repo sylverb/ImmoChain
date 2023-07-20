8f451ac408d802a3f412fb8d2bec6101120edc95
sylverb <sylver.bruneau@gmail.com>
2023-07-20 14:17:30 +0200

    Handle development and production configuration for frontend

--- immochain-app/src/main.jsx
+++ immochain-app/src/main.jsx
@@ -4,13 +4,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import { StateContextProvider } from './context';
+import dotenv from 'dotenv';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+console.log("APP_ENV = "+process.env.NODE_ENV)
+const chainId = process.env.NODE_ENV === 'production'
+  ? ChainId.Mumbai
+  : ChainId.Hardhat;
+console.log("NODE_ENV chainId = "+chainId)
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Hardhat}> 
+  <ThirdwebProvider desiredChainId={chainId}> 
     <Router>
       <StateContextProvider>
         <App />
